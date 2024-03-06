435f189368631a150cacb416fd6c2dfbfc5a29db
davidswanljvf <34351862+davidswanljvf@users.noreply.github.com>
2024-03-06 23:13:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # Импортируем функции из headhunter.py
 from headhunter import extract_max_page, extract_hh_jobs
 
-max_page = extract_max_page()  # Получаем максимальное количество страниц с объявлениями о вакансиях
-
-extract_hh_jobs(max_page)  # Извлекаем данные о вакансиях с указанного количества страниц
-#test
+hh_max_page = extract_max_page()  # Получаем максимальное количество страниц с объявлениями о вакансиях
+hh_jobs=extract_hh_jobs(hh_max_page)  # Извлекаем данные о вакансиях с указанного количества страниц
+print(hh_jobs)
