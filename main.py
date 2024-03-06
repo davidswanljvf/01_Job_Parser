4c7007f69372a67651e06a2023070f7bd597f010
davidswanljvf <ljvf@mail.ru>
2024-03-06 17:30:52 +0300

    123

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ from headhunter import extract_max_page, extract_hh_jobs
 max_page = extract_max_page()  # Получаем максимальное количество страниц с объявлениями о вакансиях
 
 extract_hh_jobs(max_page)  # Извлекаем данные о вакансиях с указанного количества страниц
+#test
